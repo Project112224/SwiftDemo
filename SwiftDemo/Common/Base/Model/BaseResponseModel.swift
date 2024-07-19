356e673b625b4ae882f4ae3ea7baa45d60a0fed9
project112224 <asd79067@gmail.com>
2024-07-19 14:30:22 +0800

    feat: add JsonBin api

--- SwiftDemo/Common/Base/Model/BaseResponseModel.swift
+++ SwiftDemo/Common/Base/Model/BaseResponseModel.swift
@@ -7,8 +7,32 @@
 
 import Foundation
 
+class JsonBinModel<T: Decodable>: Decodable {
+    let record: BaseResponseModel<T>
+    let `metadata`: BinMetadataModel
+    
+    enum CodingKeys: String, CodingKey {
+        case record
+        case `metadata`
+    }
+
+    required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.record = try container.decode(BaseResponseModel.self, forKey: .record)
+        self.metadata = try container.decode(BinMetadataModel.self, forKey: .metadata)
+    }
+}
+
 class BaseResponseModel<T: Decodable>: Decodable {
     let msgCode: String
     let msgContent: String
     let result: T
+}
+
+class BinMetadataModel: Decodable {
+    let id: String
+    let `private`: Bool
+    let name: String
+    let collectionId: String
+    let createdAt: String
 }
--- SwiftDemo/Home/ViewModel/HomeViewModel.swift
+++ SwiftDemo/Home/ViewModel/HomeViewModel.swift
@@ -10,9 +10,12 @@ import Combine
 
 class HomeViewModel {
     
-    private let firstOpenAmountApiList: [Endpoint] = [.usdSavings1, .usdFixed1, .usdDigital1, .khrSavings1, .khrFixed1, .khrDigital1]
-    private let pullRefreshAmountApiList: [Endpoint] = [.usdSavings2, .usdFixed2, .usdFixed2, .usdDigital2, .khrSavings2, .khrFixed2, .khrDigital2]
+    // .usdSavings1, .usdFixed1, .usdDigital1, .khrSavings1, .khrFixed1, .khrDigital1]
+    private let firstOpenAmountApiList: [Endpoint] = [.usdSavings1, .khrSavings1]
     
+    // .usdSavings2, .usdFixed2, .usdFixed2, .usdDigital2, .khrSavings2, .khrFixed2, .khrDigital2]
+    private let pullRefreshAmountApiList: [Endpoint] = [.usdSavings2, .khrSavings2]
+
     private var isFirstLoad: Bool = true
     
     private var notificationData: [NotificationMessageModel] = []
