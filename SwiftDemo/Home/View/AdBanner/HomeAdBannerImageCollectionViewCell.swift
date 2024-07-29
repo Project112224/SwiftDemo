f48f88fec331188cb5c83a8c5531daa3768739de
project112224 <asd79067@gmail.com>
2024-07-29 09:18:42 +0800

    feat: add open browser feature

--- SwiftDemo/Home/View/AdBanner/HomeAdBannerImageCollectionViewCell.swift
+++ SwiftDemo/Home/View/AdBanner/HomeAdBannerImageCollectionViewCell.swift
@@ -19,5 +19,16 @@ class HomeAdBannerImageCollectionViewCell: UICollectionViewCell {
     
     func configureCell(model: HomeAdBannerInfoModel) {
         self.bannerImageView.loadImage(urlString: model.linkUrl)
+        
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.openBrowser))
+        self.bannerImageView.isUserInteractionEnabled = true
+        self.bannerImageView.addGestureRecognizer(tapGestureRecognizer)
+    }
+    
+    @objc func openBrowser() {
+        let urlString: String = "https://www.google.com"
+        if let url = URL(string: urlString), UIApplication.shared.canOpenURL(url) {
+            UIApplication.shared.open(url)
+        }
     }
 }
